28dd9a1befda0f860ef331614d532522ea887a29
Mattan Alalouf <MAlalouf@mathematica-mpr.com>
2020-06-07 13:40:12 -0700

    set app to pickle results list. saved pkl file to repo for austen to play with

--- app.py
+++ app.py
@@ -6,11 +6,22 @@ Created on Sun Jun  7 12:36:58 2020
 """
 
 from GoogleNews import GoogleNews
+import pickle
 
 startdate = '02/01/2020'
 enddate = '06/07/2020'
 
-googlenews = GoogleNews(lang="en", start = startdate, end = enddate)
+# googlenews = GoogleNews(lang="en", start = startdate, end = enddate)
+googlenews = GoogleNews()
 
 
-test = googlenews.search("police")
+googlenews.search("trump")
+result = googlenews.result()
+print(len(result))
+
+print(result[0])
+
+links = [l["link"] for l in result]
+
+with open("testlist.pkl", "wb") as file:
+    pickle.dump(result, file)
